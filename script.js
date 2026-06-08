07645aa9a0265325cbb450243e892a3246a58692
CalebMillender <millendercaleb@gmail.com>
2026-06-07 21:26:08 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Display message in console
+console.log("Hello World! This is my first JavaScript output.");
+
+// Display message on webpage
+document.getElementById("output").textContent = "Hello World! JavaScript works!";
